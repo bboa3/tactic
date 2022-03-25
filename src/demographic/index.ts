4e64bc5bdb98e210cebfe3e89db7c22778581dbd
Arlindo Boa <arlindojosboa@gmail.com>
2022-03-25 20:26:14 +0300

    DEMOGRAPHICS: Get Gaza

--- src/demographic/index.ts
+++ src/demographic/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express"
 import { resolve } from "path"
 import xlsx from 'xlsx'
 import { countTotal } from "@src/demographic/count-total"
-import { getInhambane } from "@src/demographic/region/inhambane"
+import { getGaza } from "@src/demographic/region/gaza"
 
 export interface Props {
   ages: string[]
@@ -21,14 +21,14 @@ const path = resolve(__dirname, '..', '..', 'files', 'demographic', 'moçambique
 export const demographics = async (_request: Request, response: Response) => {
   const file = xlsx.readFile(path);
 
-  const firstTabName = file.SheetNames[9];
+  const firstTabName = file.SheetNames[10];
     
   const data: any = xlsx.utils.sheet_to_json(file.Sheets[firstTabName], {
     blankrows: false,
     header: 1,
   })
 
-  const people = await getInhambane(data)
+  const people = await getGaza(data)
 
   response.status(200).json(countTotal(people))
 }
