25f4897540245c6045545a1702e55c9f65e8ce0d
Arlindo Boa <arlindojosboa@gmail.com>
2022-03-25 23:56:29 +0300

    DEMOGRAPHICS: Improve Zambezia

--- src/demographic/index.ts
+++ src/demographic/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express"
 import { resolve } from "path"
 import xlsx from 'xlsx'
 import { countTotal } from "@src/demographic/count-total"
-import { getMaputoCidade } from "@src/demographic/region/maputo-cidade"
+import { getZambezia } from "@src/demographic/region/zambezia"
 
 export interface Props {
   ages: string[]
@@ -21,14 +21,14 @@ const path = resolve(__dirname, '..', '..', 'files', 'demographic', 'moçambique
 export const demographics = async (_request: Request, response: Response) => {
   const file = xlsx.readFile(path);
 
-  const firstTabName = file.SheetNames[12];
+  const firstTabName = file.SheetNames[5];
     
   const data: any = xlsx.utils.sheet_to_json(file.Sheets[firstTabName], {
     blankrows: false,
     header: 1,
   })
 
-  const people = await getMaputoCidade(data)
+  const people = await getZambezia(data)
 
   response.status(200).json(countTotal(people))
 }
--- src/demographic/region/zambezia.ts
+++ src/demographic/region/zambezia.ts
@@ -46,6 +46,78 @@ export const getZambezia = async (data: any) => {
       'POSTO ADMINISTRATIVO Cidade de Gurue': getPeople({ ages: data[0], men: data[75], women: data[76] }),
       'POSTO ADMINISTRATIVO Mepuagiua': getPeople({ ages: data[0], men: data[79], women: data[80] }),
       'POSTO ADMINISTRATIVO Lioma': getPeople({ ages: data[0], men: data[83], women: data[84] })
+    },
+    'DISTRITO Ile': {
+      'POSTO ADMINISTRATIVO Ile': getPeople({ ages: data[0], men: data[91], women: data[92] }),
+      'POSTO ADMINISTRATIVO Socone': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Inhassunge': {
+      'POSTO ADMINISTRATIVO Mucupia': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Gonhane': getPeople({ ages: data[0], men: data[100], women: data[100] })
+    },
+    'DISTRITO Lugela': {
+      'POSTO ADMINISTRATIVO Lugela-sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Tacuane': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Munhamade': getPeople({ ages: data[0], men: data[100], women: data[100] })
+    },
+    'DISTRITO Maganja da Costa': {
+      'POSTO ADMINISTRATIVO Maganja da Costa': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Nante': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Milange': {
+      'POSTO ADMINISTRATIVO Milange sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Majaua': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Mongue': getPeople({ ages: data[0], men: data[100], women: data[100] })
+    },
+    'DISTRITO Mocuba': {
+      'POSTO ADMINISTRATIVO Cidade de Mocuba': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Mugeba': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Namanjavira': getPeople({ ages: data[0], men: data[100], women: data[100] })
+    },
+    'DISTRITO Mopeia': {
+      'POSTO ADMINISTRATIVO Macuse': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Namarroi': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Morrumbala': {
+      'POSTO ADMINISTRATIVO Morrumbala-Sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Chire': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Megaza': getPeople({ ages: data[0], men: data[100], women: data[100] })
+    },
+    'DISTRITO Namacurra': {
+      'POSTO ADMINISTRATIVO Namacurra sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Macuse': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Namarroi': {
+      'POSTO ADMINISTRATIVO Namarroi': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Regone': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Nicoadala': {
+      'POSTO ADMINISTRATIVO Nicoadala sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Pebane': {
+      'POSTO ADMINISTRATIVO Pebane-Sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Mulela (Mualama)': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Naburi': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Derre': {
+      'POSTO ADMINISTRATIVO Machindo': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Guerissa': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Luabo': {
+      'POSTO ADMINISTRATIVO Samora Machel': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Chimbadzo': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Mocubela': {
+      'POSTO ADMINISTRATIVO Mocubela': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Bajone': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO Molumbo': {
+      'POSTO ADMINISTRATIVO Molumbo sede': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO Corromana': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+    },
+    'DISTRITO MULEVALA': {
+      'POSTO ADMINISTRATIVO M BAUANE': getPeople({ ages: data[0], men: data[100], women: data[100] }),
+      'POSTO ADMINISTRATIVO CHIRACO': getPeople({ ages: data[0], men: data[100], women: data[100] }),
     }
   }
 
