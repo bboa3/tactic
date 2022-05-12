55511ee863bdac1f2c6ddcb52a73b47e3af12fd0
Arlindo Boa <arlindojosboa@gmail.com>
2022-05-12 19:29:21 +0300

    Reamove Repetions

--- src/rates.ts
+++ src/rates.ts
@@ -16,19 +16,35 @@ interface InterestRates {
   'Prime rate': Rate[]
 }
 
+const filterRates = (rates: Rate[]) => {
+  const interestRates = [] as Rate[]
+
+  rates.forEach(rate => {
+    const prevRate = interestRates[interestRates.length - 1]
+
+    if (!prevRate) {
+      return interestRates.push(rate)
+    }
+
+    if (rate.value !== prevRate.value) {
+      interestRates.push(rate)
+    }
+  })
+
+  return interestRates
+} 
+
 export const rates = async (_request: Request, response: Response) => {
   const prevRates: InterestRates = JSON.parse((await fs.readFile(path, 'utf8')))
   const { FPD, FPC, "Prime rate": Prime, "Taxa MIMO": MIMO } = prevRates
   
   const interestRates = {
-    FPD: FPD.filter(rates => !!rates.value),
-    FPC: FPC.filter(rates => !!rates.value),
-    'Prime rate': Prime.filter(rates => !!rates.value),
-    'Taxa MIMO': MIMO.filter(rates => !!rates.value)
+    FPD: filterRates(FPD),
+    FPC: filterRates(FPC),
+    'Prime rate': filterRates(Prime),
+    'Taxa MIMO': filterRates(MIMO),
   }
   
-  
   await fs.writeFile(path, JSON.stringify(interestRates))
-  
   response.status(200).json(interestRates)
 }
