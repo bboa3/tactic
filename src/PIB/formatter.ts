203c7897f08e24404269c4ebd7336927f7b9cb0a
Arlindo Boa <arlindojosboa@gmail.com>
2022-06-17 01:10:39 +0300

    IMPROVE: GDP Data

--- src/PIB/formatter.ts
+++ src/PIB/formatter.ts
@@ -62,137 +62,137 @@ export const  PIBFormatter = (years: number[], pibData: PIBData) => {
     {
       id: 0,
       name: 'Agricultura, Produção Animal, Caça, Silvicultura e Pesca',
-      PIB: getPIB(years, pibLine3)
+      GDP: getPIB(years, pibLine3)
     },
     {
       id: 1,
       name: 'Agricultura, Produção Animal, Caça e Silvicultura',
-      PIB: getPIB(years, pibLine4)
+      GDP: getPIB(years, pibLine4)
     },
     {
       id: 2,
       name: 'Agricultura',
-      PIB: getPIB(years, pibLine5)
+      GDP: getPIB(years, pibLine5)
     },
     {
       id: 3,
       name: 'Produção Animal',
-      PIB: getPIB(years, pibLine6)
+      GDP: getPIB(years, pibLine6)
     },
     {
       id: 4,
       name: 'SilviCultura',
-      PIB: getPIB(years, pibLine7)
+      GDP: getPIB(years, pibLine7)
     },
     {
       id: 5,
       name: 'Pesca, Aquacultura e Actividades dos serviços relacionados',
-      PIB: getPIB(years, pibLine8)
+      GDP: getPIB(years, pibLine8)
     },
     {
       id: 6,
       name: 'Indústrias Extractivas',
-      PIB: getPIB(years, pibLine9)
+      GDP: getPIB(years, pibLine9)
     },
     {
       id: 7,
       name: 'Manufactura',
-      PIB: getPIB(years, pibLine10)
+      GDP: getPIB(years, pibLine10)
     },
     {
       id: 8,
       name: 'Produção e Distribuição de Electricidade e Gás',
-      PIB: getPIB(years, pibLine11)
+      GDP: getPIB(years, pibLine11)
     },
     {
       id: 9,
       name: 'Captação, Tratamento  e Distribuição de Água',
-      PIB: getPIB(years, pibLine12)
+      GDP: getPIB(years, pibLine12)
     },
     {
       id: 10,
       name: 'Construção',
-      PIB: getPIB(years, pibLine13)
+      GDP: getPIB(years, pibLine13)
     },
     {
       id: 11,
       name: 'Comércio, Reparação de Veículos Automóveis',
-      PIB: getPIB(years, pibLine14)
+      GDP: getPIB(years, pibLine14)
     },
     {
       id: 12,
       name: 'Transportes, Armazenagem',
-      PIB: getPIB(years, pibLine15)
+      GDP: getPIB(years, pibLine15)
     },
     {
       id: 13,
       name: 'Alojamento, Restaurantes e Similares',
-      PIB: getPIB(years, pibLine16)
+      GDP: getPIB(years, pibLine16)
     },
     {
       id: 14,
       name: 'Informação e Comunicação',
-      PIB: getPIB(years, pibLine17)
+      GDP: getPIB(years, pibLine17)
     },
     {
       id: 15,
       name: 'Actividades Financeiras',
-      PIB: getPIB(years, pibLine18)
+      GDP: getPIB(years, pibLine18)
     },
     {
       id: 16,
       name: 'Actividades Imobiliárias, Alugueres e Serviços Prestados às Empresas',
-      PIB: getPIB(years, pibLine19)
+      GDP: getPIB(years, pibLine19)
     },
     {
       id: 17,
       name: 'Administração Pública, Defesa e Segurança Social Obrigatória',
-      PIB: getPIB(years, pibLine20)
+      GDP: getPIB(years, pibLine20)
     },
     {
       id: 18,
       name: 'Educação',
-      PIB: getPIB(years, pibLine21)
+      GDP: getPIB(years, pibLine21)
     },
     {
       id: 19,
       name: 'Saúde e Acção Social',
-      PIB: getPIB(years, pibLine22)
+      GDP: getPIB(years, pibLine22)
     },
     {
       id: 20,
       name: 'Outras Actividades de Serviços Colectivos, Sociais e Pessoais',
-      PIB: getPIB(years, pibLine23)
+      GDP: getPIB(years, pibLine23)
     },
     {
       id: 21,
       name: 'Total Valores Acrescentados, preços de base',
-      PIB: getPIB(years, pibLine24)
+      GDP: getPIB(years, pibLine24)
     },
     {
       id: 22,
       name: 'Impostos sobre os Produtos',
-      PIB: getPIB(years, pibLine25)
+      GDP: getPIB(years, pibLine25)
     },
     {
       id: 23,
       name: 'IVA',
-      PIB: getPIB(years, pibLine26)
+      GDP: getPIB(years, pibLine26)
     },
     {
       id: 24,
       name: 'Direitos de Importação',
-      PIB: getPIB(years, pibLine27)
+      GDP: getPIB(years, pibLine27)
     },
     {
       id: 25,
       name: 'Outros Impostos sobre os Productos',
-      PIB: getPIB(years, pibLine28)
+      GDP: getPIB(years, pibLine28)
     },
     {
       id: 26,
       name: 'Produto Interno Bruto',
-      PIB: getPIB(years, pibLine29)
+      GDP: getPIB(years, pibLine29)
     }
   ]
 
