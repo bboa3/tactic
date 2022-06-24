a7d2acd25247d0a8b9833a2654242c1c45f3f8ad
Arlindo Boa <arlindojosboa@gmail.com>
2022-06-25 01:15:53 +0300

    FEATURE: removing first element Business Confidence Index

--- src/businessConfidence/index.ts
+++ src/businessConfidence/index.ts
@@ -17,27 +17,41 @@ export const businessConfidence = async (_request: Request, response: Response) 
     header: 1,
   }) 
 
+  const removeFirstElement = (ns: number[]) => {
+    const values = []
+    let i = 0
+
+    for (const n of ns) {
+      if (i !== 0) {
+        values.push(n)
+      }
+      i++
+    }
+
+    return values
+  }
+
   const businessConfidenceData: BusinessConfidenceData =  {
-    line2: data[2],
-    line3: data[3],
-    line4: data[4],
-    line5: data[5],
-    line7: data[7],
-    line8: data[8],
-    line9: data[9],
-    line10: data[10],
-    line12: data[12],
-    line13: data[13],
-    line14: data[14],
-    line15: data[15],
-    line17: data[17],
-    line18: data[18],
-    line19: data[19],
-    line20: data[20],
-    line22: data[22],
-    line23: data[23],
-    line24: data[24],
-    line25: data[25],
+    line2: removeFirstElement(data[2]),
+    line3: removeFirstElement(data[3]),
+    line4: removeFirstElement(data[4]),
+    line5: removeFirstElement(data[5]),
+    line7: removeFirstElement(data[7]),
+    line8: removeFirstElement(data[8]),
+    line9: removeFirstElement(data[9]),
+    line10: removeFirstElement(data[10]),
+    line12: removeFirstElement(data[12]),
+    line13: removeFirstElement(data[13]),
+    line14: removeFirstElement(data[14]),
+    line15: removeFirstElement(data[15]),
+    line17: removeFirstElement(data[17]),
+    line18: removeFirstElement(data[18]),
+    line19: removeFirstElement(data[19]),
+    line20: removeFirstElement(data[20]),
+    line22: removeFirstElement(data[22]),
+    line23: removeFirstElement(data[23]),
+    line24: removeFirstElement(data[24]),
+    line25: removeFirstElement(data[25]),
   }
 
   const businessConfidence = businessConfidenceFormatter(businessConfidenceData)
